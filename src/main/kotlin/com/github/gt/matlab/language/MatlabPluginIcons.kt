a89b02eb0990c9dd90a23966d40ef96e6a34e80b
GuillaumeTaffin <Guillaume.Taffin@opal-rt.com>
2021-12-14 11:35:15 -0500

    m file support

---
+++ src/main/kotlin/com/github/gt/matlab/language/MatlabPluginIcons.kt
@@ -0,0 +1,8 @@
+package com.github.gt.matlab.language
+
+import com.intellij.openapi.util.IconLoader
+
+object MatlabPluginIcons  {
+    @JvmField
+    val matlabIcon = IconLoader.getIcon("/icons/demoAction.png", javaClass)
+}
