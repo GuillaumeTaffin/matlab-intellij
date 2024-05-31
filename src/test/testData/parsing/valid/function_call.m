dd5ec890aee3560355f16121e9cc25216139cc99
GuillaumeTaffin <guillaume.taffinjob@gmail.com>
2024-05-30 21:14:37 -0400

    Support ref_call assignation (for arrays partial assignment)

--- src/test/testData/parsing/valid/function_call.m
+++ src/test/testData/parsing/valid/function_call.m
@@ -8,3 +8,15 @@ feval( 'disp' , 'yolo' )
 feval ('disp','yolo')
 
 z= sqrt(-9);
+
+y=data(end, 3)
+
+z = data(end - 1, 3)
+
+density = data(:, 2)
+
+volumes=data(:, end-1:end)
+q=density(2: 5);
+
+r = density([ 1 3 6])
+v2 = data(:,end)
